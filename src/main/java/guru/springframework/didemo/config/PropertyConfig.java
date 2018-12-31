916ef3831d7e1a645c43824bbd71a6547a8344e7
jonathan <jcohen66@optonline.net>
2018-12-30 19:59:47 -0500

    Indroduced multiple properties files and config classes.

--- src/main/java/guru/springframework/didemo/config/PropertyConfig.java
+++ src/main/java/guru/springframework/didemo/config/PropertyConfig.java
@@ -1,11 +1,13 @@
 package guru.springframework.didemo.config;
 
 import guru.springframework.didemo.examplebeans.FakeDataSource;
+import guru.springframework.didemo.examplebeans.FakeJmsBroker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
@@ -13,7 +15,11 @@ import org.springframework.core.env.Environment;
  * Note:  Added Environment vars GURU_USERNAME and USERNAME in run configuration.
  */
 @Configuration
-@PropertySource("classpath:datasource.properties")
+// @PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+@PropertySources({
+    @PropertySource("classpath:datasource.properties"),
+    @PropertySource("classpath:jms.properties")
+})
 public class PropertyConfig {
 
     @Autowired
@@ -28,6 +34,16 @@ public class PropertyConfig {
     @Value("${guru.dburl}")
     String url;
 
+    @Value("${guru.jms.username}")
+    String jmsUser;
+
+    @Value("${guru.jms.password}")
+    String jmsPassword;
+
+    @Value("${guru.jms.dburl}")
+    String jmsUrl;
+
+
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
@@ -38,6 +54,15 @@ public class PropertyConfig {
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
+    }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker() {
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setUser(user);
+        fakeJmsBroker.setPassword(password);
+        fakeJmsBroker.setUrl(url);
+        return fakeJmsBroker;
     }
 
     @Bean
