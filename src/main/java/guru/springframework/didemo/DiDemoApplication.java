cefb8f1d9f7aa15e1649818d5ae594ea8b690202
jonathan <jcohen66@optonline.net>
2018-12-30 19:21:08 -0500

    Created FakeDataSource and loaded it from a property file.

--- src/main/java/guru/springframework/didemo/DiDemoApplication.java
+++ src/main/java/guru/springframework/didemo/DiDemoApplication.java
@@ -4,6 +4,7 @@ import guru.springframework.didemo.controllers.ConstructorInjectedController;
 import guru.springframework.didemo.controllers.GetterInjectedController;
 import guru.springframework.didemo.controllers.MyController;
 import guru.springframework.didemo.controllers.PropertyInjectedController;
+import guru.springframework.didemo.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -18,11 +19,16 @@ public class DiDemoApplication {
 
         MyController controller = (MyController) ctx.getBean("myController");
 
+        /*
         System.out.println(controller.hello());
         System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
         System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
         System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+        */
 
+        FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+
+        System.out.println("## DB User: " + fakeDataSource.getUser());
 	}
 
 }
